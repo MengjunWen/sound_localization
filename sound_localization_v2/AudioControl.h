5669ad0590986493a3eca3d558125fcc156b4f40
Calvin Bootsman <calvinbootsman@hotmail.com>
2024-12-04 12:32:59 +0100

    option to start and stop recording by using the esp32 webserver

--- sound_localization_v2/AudioControl.h
+++ sound_localization_v2/AudioControl.h
@@ -254,7 +254,26 @@ void handleWebServer() {
                 }
                 client.print("]");
                 root.close();
-            } else if (request.indexOf("GET /") >= 0) {
+            } 
+            else if (request.indexOf("GET /start") >= 0){
+                client.println("HTTP/1.1 200 OK");
+                client.println("Content-Type: application/json");
+                client.println("Connection: close");
+                client.println();
+                client.println("{\"status\": \"Recording started\"}");
+                startRecording();
+                root.close();
+            }
+            else if (request.indexOf("GET /stop") >= 0){
+                client.println("HTTP/1.1 200 OK");
+                client.println("Content-Type: application/json");
+                client.println("Connection: close");
+                client.println();
+                client.println("{\"status\": \"Recording stopped\"}");
+                stopRecording();
+                root.close();
+            }
+            else if (request.indexOf("GET /") >= 0) {
                 // Handle file download request
                 String fileName = request.substring(5, request.indexOf(" ", 5));
                 fileName = "/" + fileName; // Ensure the file path is correct
